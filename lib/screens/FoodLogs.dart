d018cf7f89c7a08de164f0adb3f631bbc4b43828
miguelmatos-ua <miguelcruzmatos@ua.pt>
2020-06-08 22:36:45 +0100

    add dismissible on food logs

--- lib/screens/FoodLogs.dart
+++ lib/screens/FoodLogs.dart
@@ -56,28 +56,37 @@ class _FoodLogsState extends State<FoodLogs> {
   Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
     final foodLog = FoodLog.fromSnapshot(data);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(foodLog.logName),
-          subtitle: Text(foodLog.mealName),
-          trailing: Text(foodLog.mealDate),
-          leading: ConstrainedBox(
-            constraints: BoxConstraints(
-              minWidth: 100,
-              minHeight: 100,
-              maxWidth: 200,
-              maxHeight: 200,
-            ),
-            child:
-                Image.network(foodLog.pictureUrl.toString(), fit: BoxFit.cover),
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (_) async {
+        await Firestore.instance
+            .runTransaction((Transaction myTransaction) async {
+          await myTransaction.delete(data.reference);
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
           ),
-          onTap: () => print('Clicked food log'),
+          child: ListTile(
+            title: Text(foodLog.logName),
+            subtitle: Text(foodLog.mealName),
+            trailing: Text(foodLog.mealDate),
+            leading: ConstrainedBox(
+              constraints: BoxConstraints(
+                minWidth: 100,
+                minHeight: 100,
+                maxWidth: 200,
+                maxHeight: 200,
+              ),
+              child: Image.network(foodLog.pictureUrl.toString(),
+                  fit: BoxFit.cover),
+            ),
+            onTap: () => print('Clicked food log'),
+          ),
         ),
       ),
     );
